cc2c0a6efd48f0b1ca14924dc5a94eb42ece84ed
wandl <hnxyhcwdl1003@163.com>
2020-08-24 20:01:20 +0800

    有些

--- src/main/java/org/springframework/security/boot/SecurityUnionIDAutoConfiguration.java
+++ src/main/java/org/springframework/security/boot/SecurityUnionIDAutoConfiguration.java
@@ -1,14 +1,17 @@
 package org.springframework.security.boot;
 
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.boot.biz.userdetails.JwtPayloadRepository;
 import org.springframework.security.boot.biz.userdetails.UserDetailsServiceAdapter;
 import org.springframework.security.boot.unionid.authentication.UnionIDAuthenticationProvider;
 import org.springframework.security.boot.unionid.authentication.UnionIDMatchedAuthenticationEntryPoint;
 import org.springframework.security.boot.unionid.authentication.UnionIDMatchedAuthenticationFailureHandler;
+import org.springframework.security.boot.unionid.authentication.UnionIDMatchedAuthenticationSuccessHandler;
 
 @Configuration
 @AutoConfigureBefore(SecurityBizAutoConfiguration.class)
@@ -17,16 +20,26 @@ import org.springframework.security.boot.unionid.authentication.UnionIDMatchedAu
 public class SecurityUnionIDAutoConfiguration {
 
 	@Bean
+	@ConditionalOnMissingBean
 	public UnionIDMatchedAuthenticationEntryPoint unionidMatchedAuthenticationEntryPoint() {
 		return new UnionIDMatchedAuthenticationEntryPoint();
 	}
 
 	@Bean
+	@ConditionalOnMissingBean
 	public UnionIDMatchedAuthenticationFailureHandler unionidMatchedAuthenticationFailureHandler() {
 		return new UnionIDMatchedAuthenticationFailureHandler();
 	}
-
+	
+	
 	@Bean
+	@ConditionalOnMissingBean
+	public UnionIDMatchedAuthenticationSuccessHandler unionidMatchedAuthenticationSuccessHandler(JwtPayloadRepository payloadRepository) {
+		return new UnionIDMatchedAuthenticationSuccessHandler(payloadRepository);
+	}
+	
+	@Bean
+	@ConditionalOnMissingBean
 	public UnionIDAuthenticationProvider unionidAuthenticationProvider(UserDetailsServiceAdapter userDetailsService) {
 		return new UnionIDAuthenticationProvider(userDetailsService);
 	}
---
+++ src/main/java/org/springframework/security/boot/unionid/authentication/UnionIDMatchedAuthenticationSuccessHandler.java
@@ -0,0 +1,77 @@
+package org.springframework.security.boot.unionid.authentication;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.boot.biz.SpringSecurityBizMessageSource;
+import org.springframework.security.boot.biz.authentication.nested.MatchedAuthenticationSuccessHandler;
+import org.springframework.security.boot.biz.exception.AuthResponse;
+import org.springframework.security.boot.biz.exception.AuthResponseCode;
+import org.springframework.security.boot.biz.userdetails.JwtPayloadRepository;
+import org.springframework.security.boot.biz.userdetails.UserProfilePayload;
+import org.springframework.security.boot.utils.SubjectUtils;
+import org.springframework.security.core.Authentication;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * UnionID 认证 (authentication)成功回调器：讲认证信息写回前端
+ * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ */
+public class UnionIDMatchedAuthenticationSuccessHandler implements MatchedAuthenticationSuccessHandler {
+   
+	protected MessageSourceAccessor messages = SpringSecurityBizMessageSource.getAccessor();
+	private JwtPayloadRepository payloadRepository;
+	private boolean checkExpiry = false;
+	
+	public UnionIDMatchedAuthenticationSuccessHandler(JwtPayloadRepository payloadRepository) {
+		this.setPayloadRepository(payloadRepository);
+	}
+	
+	@Override
+	public boolean supports(Authentication authentication) {
+		return SubjectUtils.isAssignableFrom(authentication.getClass(), UnionIDAuthenticationToken.class);
+	}
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
+    	
+    	// 设置状态码和响应头
+		response.setStatus(HttpStatus.OK.value());
+		response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+		response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+		// 国际化后的异常信息
+		String message = messages.getMessage(AuthResponseCode.SC_AUTHC_SUCCESS.getMsgKey(), LocaleContextHolder.getLocale());
+		// 写出JSON
+		UserProfilePayload profilePayload = getPayloadRepository().getProfilePayload((AbstractAuthenticationToken) authentication, isCheckExpiry());
+		JSONObject.writeJSONString(response.getWriter(), AuthResponse.success(message, profilePayload));
+    	 
+    }
+    
+	public JwtPayloadRepository getPayloadRepository() {
+		return payloadRepository;
+	}
+
+	public void setPayloadRepository(JwtPayloadRepository payloadRepository) {
+		this.payloadRepository = payloadRepository;
+	}
+
+	public boolean isCheckExpiry() {
+		return checkExpiry;
+	}
+
+	public void setCheckExpiry(boolean checkExpiry) {
+		this.checkExpiry = checkExpiry;
+	}
+
+}
